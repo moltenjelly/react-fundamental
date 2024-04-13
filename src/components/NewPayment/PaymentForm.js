3e2041a698c5af9223259ea05738e80b880486c3
moltenjelly <dongha9876@gmail.com>
2024-04-13 15:50:19 +0900

    Update PaymentForm.js

--- src/components/NewPayment/PaymentForm.js
+++ src/components/NewPayment/PaymentForm.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './PaymentForm.css';
+import "./PaymentForm.css";
 
 const PaymentForm = () => {
+  const [objectState, setObjectState] = useState({
+    password: "",
+    radio: "",
+    color: "#ffffff",
+  });
+
+  const inputTypePasswordChangeHandler = (event) => {
+    setObjectState((prevState) => ({
+      ...prevState,
+      password: event.target.value,
+    }));
+  };
+  const inputTypeRadioChangeHandler = (event) => {
+    setObjectState((prevState) => ({
+      ...prevState,
+      radio: event.target.value,
+    }));
+  };
+  const inputTypeColorChangeHandler = (event) => {
+    setObjectState((prevState) => ({
+      ...prevState,
+      today: event.target.value,
+    }));
+  };
+  const buttonSubmitHandler = (event) => {
+    console.log("password", objectState.password);
+    console.log("radio", objectState.radio);
+    console.log("color", objectState.color);
+  };
+
   return (
     <form>
-      <div className='new-payment__controls'>
-        <div className='new-payment__control'>
-          <label>이름</label>
-          <input type='text' />
+      <div className="new-payment__controls">
+        <div className="new-payment__control">
+          <label>비밀번호</label>
+          <input
+            type="password"
+            value={objectState.password}
+            onChange={inputTypePasswordChangeHandler}
+          />
         </div>
-        <div className='new-payment__control'>
-          <label>금액</label>
-          <input type='number' min='0.01' step='0.01' />
+        <div className="new-payment__control">
+          <label>라디오</label>
+          <input
+            type="radio"
+            value={objectState.radio}
+            onChange={inputTypeRadioChangeHandler}
+          />
         </div>
-        <div className='new-payment__control'>
-          <label>날짜</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+        <div className="new-payment__control">
+          <label>Color</label>
+          <input
+            type="color"
+            value={objectState.color}
+            onChange={inputTypeColorChangeHandler}
+          />
         </div>
       </div>
-      <div className='new-payment__actions'>
-        <button type='submit'>결제 추가</button>
+      <div className="new-payment__actions">
+        <button type="button" onClick={buttonSubmitHandler}>
+          결제 추가
+        </button>
       </div>
     </form>
   );
